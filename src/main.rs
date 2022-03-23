5bd244c00969abbb5a81a0fc36193fda8dedf0d8
Ben Kimock <kimockb@gmail.com>
2022-03-23 19:40:20 -0400

    Save location in URL

--- src/main.rs
+++ src/main.rs
@@ -256,6 +256,7 @@ pre {{
     word-wrap: break-word;
     white-space: pre-wrap;
     font-size-adjust: none;
+    font-size: 14px;
 }}
 </style><title>{} {}</title></head><body><pre>
 {}
@@ -276,7 +277,8 @@ fn write_crate_output(krate: &Crate, output: &str) {
     .unwrap();
 }
 
-const OUTPUT_HEADER: &str = r#"<html><head><style>
+const OUTPUT_HEADER: &str = r#"<!DOCTYPE HTML>
+<html><head><style>
 body {
     background: #111;
     color: #eee;
@@ -323,18 +325,69 @@ a {
     display: flex;
     flex-direction: row;
     width: 100%;
+    height: 100%;
     margin: 0;
 }
-</style></head><body>
+</style></head><body onload="init()">
 <script>
-function change_log(path) {
-    var html = "<object data=\"" + path + "\" width=100% height=100%></object>";
-    document.getElementById("log").innerHTML = html;
+function init() {
+    var params = decode_params();
+    if (params.crate != undefined && params.version != undefined) {
+        change_log(params.crate, params.version);
+    }
+}
+function decode_params() {
+    var params = {};
+    var paramsarr = window.location.search.substr(1).split('&');
+    for (var i = 0; i < paramsarr.length; ++i) {
+        var tmp = paramsarr[i].split("=");
+        if (!tmp[0] || !tmp[1]) continue;
+        params[tmp[0]]  = decodeURIComponent(tmp[1]);
+    }
+    return params;
+}
+function change_log(crate, version) {
+    var html = "<object data=\"logs/" + crate + "/" + version + ".html\" width=100% height=100%></object>";
+    var build_log = document.getElementById("log");
+    build_log.innerHTML = html;
+
+    params = decode_params();
+    params.crate = crate;
+    params.version = version;
+    history.replaceState(null, null, encode_params(params));
+}
+function encode_params(params) {
+    var uri = "?";
+    for (var key in params) {
+        uri += key + '=' + encodeURIComponent(params[key]) + '&';
+    }
+    if (uri.slice(-1) == "&") {
+        uri = uri.substring(0, uri.length - 1);
+    }
+    if (uri == '?') {
+        uri = window.location.href.split('?')[0];
+    }
+    return uri;
 }
 </script>
 <div class="page">
 <div class="log" id="log">
-    <object width=100% height=100%></object>
+<object>
+<html><head><style>
+body {
+    background: #111;
+    color: #eee;
+}
+pre {
+    word-wrap: break-word;
+    white-space: pre-wrap;
+    font-size-adjust: none;
+    font-size: 14px;
+}
+</style><title>Miri build logs</title></head><body><pre>
+Click on a crate to the right to display its build log
+</pre></body></html>
+</object>
 </div>
 <div class="crates">
 "#;
@@ -353,7 +406,7 @@ fn write_output(crates: &[Crate]) {
     for c in crates {
         write!(
             output,
-            "<div class=\"row\" onclick=\"change_log(&quot;logs/{}/{}.html&quot;)\"><div class=\"crate\">{} {}</div>",
+            "<div class=\"row\" onclick=\"change_log(&quot;{}&quot;, &quot;{}&quot;)\"><div class=\"crate\">{} {}</div>",
             c.name, c.version, c.name, c.version
         )
         .unwrap();
@@ -377,7 +430,7 @@ fn write_output(crates: &[Crate]) {
         if let Status::UB { cause, .. } = &c.status {
             write!(
             output,
-            "<div class=\"row\" onclick=\"change_log(&quot;logs/{}/{}.html&quot;)\"><div class=\"crate\">{} {}</div>",
+            "<div class=\"row\" onclick=\"change_log(&quot;{}&quot;, &quot;{}&quot;)\"><div class=\"crate\">{} {}</div>",
             c.name, c.version, c.name, c.version
         )
             .unwrap();
