02e325a1454693f92d9512c32217d7027a0edb5b
Ben Kimock <kimockb@gmail.com>
2022-07-19 17:09:53 -0400

    Restore colors in output, via hackery

--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,7 @@
 exec 2>&1
 
+export TERM=xterm-256color
+
 while read crate;
 do
     cd /root # ensure that we don't remove the current directory
@@ -9,8 +11,8 @@ do
         cd /root/build
         cargo +miri update --color=always
         cargo +miri miri test --no-run --color=always
-        /usr/bin/time -v timeout $TEST_TIMEOUT cargo +miri miri test --color=always --no-fail-fast -- --test-threads=2
+        unbuffer -p /usr/bin/time -v timeout $TEST_TIMEOUT cargo +miri miri test --no-fail-fast -- --test-threads=2
         cat Cargo.lock
-        echo $TEST_END_DELIMITER
     fi
+    echo $TEST_END_DELIMITER
 done < /dev/stdin
