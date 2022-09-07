ed46815d8562ac1f59686b8aaa5008f154b53824
Ben Kimock <kimockb@gmail.com>
2022-09-07 11:15:00 -0400

    Update to new directory scheme

--- docker/run.sh
+++ docker/run.sh
@@ -4,11 +4,10 @@ export TERM=xterm-256color
 
 while read crate;
 do
-    cd /root # ensure that we don't remove the current directory
-    rm -rf /root/build # clean out anything from an old build (probably)
+    cd /root/build
+    rm -rf /root/build{*,.*} # clean out anything from an old build (probably)
     if cargo download $crate /root/build
     then
-        cd /root/build
         cargo +miri update --color=always
         cargo +miri miri test --no-run --color=always --jobs=1
         unbuffer -p /usr/bin/time -v timeout $TEST_TIMEOUT cargo +miri miri test --no-fail-fast -- --test-threads=2
