24b5e1a4638c1fe9c019ca8aec313690b6d284ff
Ben Kimock <kimockb@gmail.com>
2023-07-23 00:20:46 -0400

    Migrate helper programs to Rust

---
+++ inapty/src/main.rs
@@ -0,0 +1,32 @@
+use std::fs::File;
+use std::os::fd::FromRawFd;
+use std::os::unix::process::CommandExt;
+use std::process::Command;
+use std::ptr;
+
+fn main() {
+    let mut pty: i32 = 0;
+    // SAFETY: No preconditions
+    let pid = unsafe { libc::forkpty(&mut pty, ptr::null_mut(), ptr::null(), ptr::null()) };
+
+    if pid == 0 {
+        // We are the child. Spawn the subprocess based off our arguments.
+        let mut args = std::env::args_os().skip(1);
+        Command::new(args.next().unwrap()).args(args).exec();
+        unreachable!("exec should not return");
+    } else {
+        // We are the originating process. Copy from the pty to output.
+        // SAFETY: master is open and valid, it was just opened by forkpty
+        let mut pty = unsafe { File::from_raw_fd(pty) };
+        // Copy all the output from the child pty to our stdout
+        while std::io::copy(&mut pty, &mut std::io::stdout()).is_ok() {}
+        // Exit according to our child's status
+        // SAFETY: No preconditions
+        let status = unsafe {
+            let mut status = 0;
+            libc::waitpid(pid, &mut status, 0);
+            libc::WEXITSTATUS(status)
+        };
+        std::process::exit(status);
+    }
+}
