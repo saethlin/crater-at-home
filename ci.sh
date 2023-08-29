a2a436987ffaa663db64d9f698fbf5d22587243e
Ben Kimock <kimockb@gmail.com>
2023-08-29 19:51:53 -0400

    Use log groups

--- ci.sh
+++ ci.sh
@@ -4,6 +4,16 @@ set -e
 exec 2>&1
 export TERM=xterm-256color
 
+function begingroup {
+    echo "::group::$@"
+    set -x
+}
+
+function endgroup {
+    set +x
+    echo "::endgroup"
+}
+
 function style() {
     cargo fmt --check
     cargo clippy -- -Dclippy::all
@@ -24,8 +34,18 @@ function build() {
     cargo run -- run --tool=build --bucket=miri-bot-dev --crate-list=ci-crates --rerun
 }
 
+begingroup Update toolchain
+
 rustup self update
 rustup default stable
 rustup update
+
+endgroup
+
+if $1 -ne "style" then
+    begingroup cargo build
+    cargo build
+    endgroup
+fi
 
 $1
