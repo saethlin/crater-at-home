49930d881206e451d43d5ef1d27d9fdb7d9586a3
Ben Kimock <kimockb@gmail.com>
2023-09-02 12:58:25 -0400

    Use caching

--- ci.sh
+++ ci.sh
@@ -4,27 +4,11 @@ set -e
 exec 2>&1
 export TERM=xterm-256color
 
-function begingroup {
-    echo "::group::$@"
-}
-
-function endgroup {
-    echo "::endgroup"
-}
-
 function group {
     echo "::group::$@"
     $@
     echo "::endgroup"
 }
-
-begingroup Update toolchain
-
-rustup self update
-rustup default stable
-rustup update
-
-endgroup
 
 if [[ "$1" == "style" ]]
 then
@@ -38,9 +22,6 @@ then
     group cargo +nightly fuzz run render -- -runs=100000
 else
     group cargo build
-
-    begingroup "Test --tool=$1"
-    cargo run -- sync --tool="$1" --bucket=miri-bot-dev
-    cargo run -- run --tool="$1" --bucket=miri-bot-dev --crate-list=ci-crates --rerun
-    endgroup
+    group cargo run -- sync --tool="$1" --bucket=miri-bot-dev
+    group cargo run -- run --tool="$1" --bucket=miri-bot-dev --crate-list=ci-crates --rerun
 fi
